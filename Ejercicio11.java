4238c5fb77f8ac11f31c04f2e4a281d5da1024cb
Santiago Gutierrez <119289025+Santiago1721@users.noreply.github.com>
2023-01-25 14:13:22 +0100

    Create Ejercicio11.java

---
+++ Ejercicio11.java
@@ -0,0 +1,20 @@
+package ejercicios16deEnero;
+
+public class Ejercicio11 {
+
+	public static void main(String[] args) {
+		char simbolo [] = new char[12];
+		
+		simbolo[0] = 'a';
+		simbolo[1] = 'x';
+		simbolo[4] = '@';
+		simbolo[6] = ' ';
+		simbolo[7] = '+';
+		simbolo[8] = 'Q';
+		
+		for(int i = 0;i < 12; i++) {
+			System.out.println(simbolo[i]);
+		}
+	}
+
+}
