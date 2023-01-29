ab31a3c6b0cdfe3f6f00da54b924c021415e8f92
Santiago Gutierrez <119289025+Santiago1721@users.noreply.github.com>
2023-01-29 20:32:36 +0100

    Update Ejercicio6.java

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,29 +1,22 @@
-public class Ejercicio6 {
+package EjerciciosArraysObligatorios;
 
-	public static void main(String[] args) throws InterruptedException {
-		int[][] n = new int [6][6];
-		n[0][0]= 3;
-		n[0][1]= 5;
-		n[2][1]= 8;
-		n[2][2]= 7;
-		n[3][2]= 7;
-		n[3][3]= 3;
-		n[4][3]= 9;
-		n[4][4]= 5;
-		n[5][4]= 10;
-		n[5][5]= 9;
-		
-		
-		int fila, columna;
-		for(fila = 0; fila <6; fila++){
-			
-			System.out.print("Fila: " + fila);
-			for(columna = 0; columna < 2; columna++) {
-			System.out.printf("%10d ", n[fila][columna]);
-			Thread.sleep(1000);                                   // retardo de un segundo
-			}
-			System.out.println();
-			}
-			}
-			}
+public class Ejercicio6 {
+	  public static void main(String[] args) {
+	    int[][] num = new int[3][6];
+	    // Assign values to the elements of the array
+	    for (int i = 0; i < num.length; i++) {
+	      for (int j = 0; j < num[i].length; j++) {
+	        num[i][j] = i + j;
+	      }
+	    }
+	    // Display the elements of the array in table form
+	    System.out.println("Tabla de elementos del array 'num':");
+	    for (int i = 0; i < num.length; i++) {
+	      for (int j = 0; j < num[i].length; j++) {
+	        System.out.print(num[i][j] + " ");
+	      }
+	      System.out.println();
+	    }
+	  }
+	}
 		
